85aa259599c797864c33cd1c787ce87c1526d402
yattdev <yattdeveloper@gmail.com>
2021-11-02 00:26:56 +0100

    Create and initialise PochetteDetails vue

---
+++ src/views/PochetteDetails.vue
@@ -0,0 +1,51 @@
+<template>
+<!-- Page Content -->
+<div class="container mb-10">
+    <!-- Portfolio Item Heading -->
+    <h1 class="my-4">Page Heading
+      <small>Secondary Text</small>
+    </h1>
+
+    <!-- Portfolio Item Row -->
+    <div class="row">
+
+      <div class="col-md-6 mr-5">
+        <img class="img-fluid" src="https://via.placeholder.com/550x200" alt="">
+      </div>
+
+      <div class="col-md-4">
+        <h3 class="my-3">Project Description</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
+        <h3 class="my-3">Project Details</h3>
+        <ul>
+          <li>Lorem Ipsum</li>
+          <li>Dolor Sit Amet</li>
+          <li>Consectetur</li>
+          <li>Adipiscing Elit</li>
+        </ul>
+      </div>
+
+    </div>
+    <!-- /.row -->
+  </div>
+  <!-- /.container -->
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-property-decorator';
+import axios from "axios"
+
+export default Vue.component('PochetteDetail', {
+    name: 'PochetteDetail',
+    data() {
+        return {
+        };
+    },
+    mounted(){
+    },
+    methods: {
+
+    },
+
+});
+</script>
